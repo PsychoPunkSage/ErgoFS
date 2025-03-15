ea428d4ca69c93ae0226d6d9f76d73e47435098d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2025-03-15 13:06:50 +0530

    fix:: Finally completed all the missing impl

--- pkg/types/buffer.go
+++ pkg/types/buffer.go
@@ -317,7 +317,7 @@ func BfindForAttach(bmgr *BufferManager, btype int, size uint64,
 			panic("BUG: usedBefore != (cur.Buffers.Off & (blksiz - 1))")
 		}
 
-		ret = BattachInternalExtend(cur, nil, size, alignsize, requiredExt+inlineExt, true)
+		ret = BattachInternal(cur, nil, size, alignsize, requiredExt+inlineExt, true)
 		if ret < 0 {
 			// panic("BUG: ret < 0")
 			continue
@@ -362,7 +362,7 @@ skipMapped:
 			continue
 		}
 
-		ret = BattachInternalExtend(cur, nil, size, alignsize, requiredExt+inlineExt, true)
+		ret = BattachInternal(cur, nil, size, alignsize, requiredExt+inlineExt, true)
 		if ret < 0 {
 			nextEntry := cur.List.Next
 			nextInterface := ContainerOf(nextEntry, &BufferBlock{}, "List")
@@ -574,4 +574,105 @@ func ContainerOf1(ptr, sample interface{}, member string) interface{} {
 
 func RoundDown(x, y int) int {
 	return x - (x % y)
+}
+
+// BupdateMapped updates the mapped buffer block
+func BupdateMapped(bb *BufferBlock) {
+	bmgr := bb.Buffers.FsPrivate.(*BufferManager)
+	sbi := bmgr.Sbi
+	var bkt *ListHead
+
+	if bb.BlkAddr == NULL_ADDR {
+		return
+	}
+
+	bucketIndex := int(bb.Buffers.Off & uint64(ErofsBlkSiz(sbi)-1))
+	bkt = &bmgr.MappedBuckets[bb.Type][bucketIndex]
+
+	ListDel(&bb.MappedList)
+	ListAddTail(&bb.MappedList, bkt)
+}
+
+// BattachInternal attaches a buffer head to a buffer block and returns occupied bytes if successful
+func BattachInternal(bb *BufferBlock, bh *BufferHead, incr uint64,
+	alignsize, extrasize uint32, dryrun bool) int {
+	bmgr := bb.Buffers.FsPrivate.(*BufferManager)
+	sbi := bmgr.Sbi
+	blksiz := ErofsBlkSiz(sbi)
+	blkmask := blksiz - 1
+	boff := bb.Buffers.Off
+	alignedoffset := RoundUp(boff, uint64(alignsize))
+
+	// Calculate the "out of bounds" condition
+	prevBlockOffset := ((boff - 1) & uint64(blkmask)) + 1
+	roundedPrevOffset := RoundUp(prevBlockOffset, uint64(alignsize))
+	totalSize := roundedPrevOffset + incr + uint64(extrasize)
+	oob := Cmpsgn(totalSize, uint64(blksiz))
+
+	var tailupdate bool
+	var blkaddr uint32
+
+	if oob >= 0 {
+		// The next buffer block should be NULL_ADDR all the time
+		if oob > 0 {
+			nextEntry := bb.List.Next
+			nextInterface := ContainerOf(nextEntry, &BufferBlock{}, "List")
+			next, _ := nextInterface.(*BufferBlock)
+
+			if next.BlkAddr != NULL_ADDR {
+				return EINVAL
+			}
+		}
+
+		blkaddr = bb.BlkAddr
+		if blkaddr != NULL_ADDR {
+			tailupdate = (bmgr.TailBlkAddr == blkaddr+
+				uint32(BlkRoundUp(sbi, boff)))
+
+			if oob > 0 && !tailupdate {
+				return EINVAL
+			}
+		}
+	}
+
+	if !dryrun {
+		if bh != nil {
+			bh.Off = alignedoffset
+			bh.Block = bb
+			ListAddTail(&bh.List, &bb.Buffers.List)
+		}
+		boff = alignedoffset + incr
+		bb.Buffers.Off = boff
+		// Need to update the tail_blkaddr
+		if tailupdate {
+			bmgr.TailBlkAddr = blkaddr + uint32(BlkRoundUp(sbi, boff))
+		}
+		BupdateMapped(bb)
+	}
+
+	return int(((alignedoffset + incr - 1) & uint64(blkmask)) + 1)
+}
+
+// Roundup rounds up a number to the nearest multiple of align
+func RoundUp(x, y uint64) uint64 {
+	if y == 0 {
+		return x // Avoid division by zero
+	}
+	return ((x + (y - 1)) / y) * y
+}
+
+// Cmpsgn compares two numbers and returns sign (negative, zero, positive)
+func Cmpsgn(a, b uint64) int {
+	if a < b {
+		return -1
+	} else if a > b {
+		return 1
+	}
+	return 0
+}
+
+// BlkRoundUp rounds up offset to the next block
+func BlkRoundUp(sbi *SuperBlkInfo, offset uint64) uint64 {
+	blksz := ErofsBlkSiz(sbi)
+	return RoundUp(offset+uint64(blksz-1), uint64(blksz)) >> sbi.BlkSzBits
 }
